409bad76d06913c1c098801ab3dbfddb4b50df94
NikQuila <nicopirozzi1@gmail.com>
2020-07-19 01:10:31 -0400

    Updates

---
+++ precache-manifest.77c5ceb20849f63004927c675635dce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b399a648aaa399e958200e6555a0c3e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70df8970edca9b049bda",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d9855bef94b77eea0f2c",
+    "url": "/static/css/main.6ca2ba62.chunk.css"
+  },
+  {
+    "revision": "70df8970edca9b049bda",
+    "url": "/static/js/2.76bfa2e2.chunk.js"
+  },
+  {
+    "revision": "ca429a12ee98e43556d1195e6b1d1e1e",
+    "url": "/static/js/2.76bfa2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9855bef94b77eea0f2c",
+    "url": "/static/js/main.b6bc85d9.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
